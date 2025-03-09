d4ab07457293d60e2bd4f8aa2f68634eaac27122
angeloje <angeloje97@outlook.com>
2025-03-08 23:34:28 -0800

    3/8/2025 Created the settings

---
+++ Source/ColonyConditioning/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace ColonistConditioning
+{
+    class Manager: Mod
+    {
+        public static Settings settings;
+
+        public override string SettingsCategory() => "Colony Conditioning";
+
+        public Manager(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<Settings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            var listingStandard = new Listing_Standard();
+
+            #region Settings List
+            listingStandard.Begin(inRect);
+
+            settings.HandleListing(listingStandard, inRect);
+
+            listingStandard.End();
+            #endregion
+
+            listingStandard.End();
+            settings.Write();
+        }
+    }
+}
---
+++ Source/ColonyConditioning/Settings.cs
@@ -0,0 +1,79 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace ColonistConditioning
+{
+    public class Settings : ModSettings
+    {
+        [Range(0f, 100f)] float traitGainMultiplier = 100f;
+
+        public float TraitGainMultiplier => traitGainMultiplier;
+
+        [Range(0f, 100f)]float traitLossMultiplier = 100f;
+        public float TraitLossMultiplier => traitLossMultiplier;
+
+        bool worsenWhenNeutral = true;
+
+        public bool WorsenWhenNeutral => worsenWhenNeutral;
+
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref traitGainMultiplier, "traitGainMultiplier", 100f);
+            Scribe_Values.Look(ref traitLossMultiplier, "traitLossMultiplier", 100f);
+
+        }
+
+        public void HandleListing(Listing_Standard listing, Rect rect)
+        {
+            var rowPos = 0f;
+            HandleFloat(ref traitGainMultiplier, $"Trait gain multiplier: {traitGainMultiplier}%");
+            HandleFloat(ref traitLossMultiplier, $"Trait loss multiplier: {traitLossMultiplier}%");
+            
+            HandleBool(ref worsenWhenNeutral, "Worsen on neutral");
+
+            #region Handlers
+
+            void HandleFloat(ref float val, string label)
+            {
+                var nextRect = NextRect();
+
+                nextRect.width *= .5f;
+                
+                Widgets.HorizontalSlider(
+                    nextRect,
+                    ref val,
+                    new FloatRange(0f, 200f),
+                    label,
+                    roundTo: 1
+                 );
+            }
+
+            void HandleBool(ref bool val, string label, string toolTip = "")
+            {
+                var nextRect = NextRect(0f);
+                nextRect.width *= .5f;
+                Widgets.CheckboxLabeled(nextRect, label, ref (val));
+                //listing.CheckboxLabeled(label, ref val, "This will determine whether or not a pawn will progress to get negative traits" +
+                //    "once their conditioning becomes neutral.");
+            }
+
+            Rect NextRect(float height = 30f)
+            {
+                rowPos += height;
+                return listing.GetRect(rowPos);
+            }
+            
+            #endregion
+        }
+
+    }
+}
