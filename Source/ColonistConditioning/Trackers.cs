10908026fc967a606e19150b3cd09d5cd52efce2
angeloje <angeloje97@outlook.com>
2025-03-09 17:28:51 -0700

    Added the tracking capability.

---
+++ Source/ColonistConditioning/Trackers.cs
@@ -0,0 +1,108 @@
+﻿using ColonistConditioning;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace ColonyConditioning
+{
+    #region Harmony
+
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        public static DistanceTracker DistanceTrackerInstance = new DistanceTracker();
+
+        static HarmonyPatches()
+        {
+            var harmony = new Harmony(Utility.UID);
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Pawn))]
+    [HarmonyPatch("Tick")]
+
+    public static class Pawn_Tick_Patch
+    {
+        static void Postfix(Pawn __instance)
+        {
+            if (__instance.IsColonist)
+            {
+                HarmonyPatches.DistanceTrackerInstance.Update(__instance);
+            }
+        }
+    }
+
+    #endregion
+
+    #region Trackers
+    public class TrackerManager
+    {
+
+    }
+
+    public class Tracker<T> : IExposable
+    {
+        public virtual string label => "Generic Tracker";
+
+        public Dictionary<Pawn, T> pawnValues = new Dictionary<Pawn, T>();
+
+        public void SetValue(Pawn pawn, T value)
+        {
+            pawnValues[pawn] = value;
+        }
+
+        public virtual void Update(Pawn pawn) { }
+
+        public virtual void ExposeData()
+        {
+            Scribe_Collections.Look(ref pawnValues, label, LookMode.Reference, LookMode.Value);
+
+            if(Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                if(pawnValues == null)
+                {
+                    pawnValues = new Dictionary<Pawn, T>();
+                }
+            }
+        }
+    }
+    
+    public class DistanceTracker: Tracker<float>
+    {
+        public override string label => "Distance Tracker";
+        public Dictionary<Pawn, IntVec3> lastPositions = new Dictionary<Pawn, IntVec3>();
+
+        public override void Update(Pawn pawn)
+        {
+            if (!Utility.TickElapsed(pawn)) return;
+            var newPosition = pawn.Position;
+            if (!lastPositions.ContainsKey(pawn))
+            {
+                lastPositions[pawn] = newPosition;
+                if (!pawnValues.ContainsKey(pawn))
+                {
+                    pawnValues[pawn] = 0;
+                }
+                return;
+            }
+
+            if (newPosition == lastPositions[pawn]) return;
+            pawnValues[pawn] += (lastPositions[pawn] - newPosition).LengthHorizontal;
+            lastPositions[pawn] = newPosition;
+            Log.Message($"{pawn} Traveled {pawnValues[pawn]} Distance");
+        }
+    }
+
+    #endregion
+    
+    #region Milestones
+
+
+    #endregion
+}
---
+++ Source/ColonistConditioning/Utility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace ColonistConditioning
+{
+    public static class Utility
+    {
+        #region Properties
+
+        public static string UID => "com.stormdexter.colonistconditioning";
+
+        #endregion
+
+        readonly static Dictionary<Tuple<Pawn, string>, int> tickCounters = new Dictionary<Tuple<Pawn, string>, int>();
+        readonly static int tickInterval = 60;
+        public static bool TickElapsed(Pawn pawn, [CallerMemberName] string methodName = null, [CallerLineNumber] int lineNumber = 0)
+        {
+            var key = new Tuple<Pawn, string>(pawn, methodName);
+            if(!tickCounters.ContainsKey(key))
+            {
+                tickCounters[key] = 1;
+                return false;
+            }
+
+            if (tickCounters[key] < tickInterval)
+            {
+                tickCounters[key]++;
+                return false;
+            }
+
+            tickCounters[key] = 0;
+
+            return true;
+        }
+    }
+
+}
